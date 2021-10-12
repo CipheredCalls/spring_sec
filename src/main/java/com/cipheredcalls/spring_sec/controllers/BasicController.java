d1707aeb425d258e6179348250a16a1d700584d6
Mohammed Balhaddad <mbailhadadd@gmail.com>
2021-10-12 08:16:28 +0300

    finish lesson 4

---
+++ src/main/java/com/cipheredcalls/spring_sec/controllers/BasicController.java
@@ -0,0 +1,19 @@
+package com.cipheredcalls.spring_sec.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+    @GetMapping("/")
+    public ResponseEntity<?> get(){
+        return ResponseEntity.ok("Hi , I am a get");
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> post(){
+        return ResponseEntity.ok("Hi , I am a post");
+    }
+}
