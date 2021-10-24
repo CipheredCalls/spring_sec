c9d350699337732d7f3f469bdc93cf025a5e4ca8
Mohammed Balhaddad <mbailhadadd@gmail.com>
2021-10-24 10:53:10 +0300

    finish lesson 9

--- src/main/java/com/cipheredcalls/spring_sec/configs/SecurityConfigs.java
+++ src/main/java/com/cipheredcalls/spring_sec/configs/SecurityConfigs.java
@@ -2,6 +2,8 @@ package com.cipheredcalls.spring_sec.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,9 +11,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class SecurityConfigs {
-    @Bean
-    UserDetailsService userDetailsService(){
+public class SecurityConfigs extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetails())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    UserDetailsService userDetails(){
         var userDetailsService = new InMemoryUserDetailsManager();
 
         userDetailsService.createUser(
@@ -23,8 +32,6 @@ public class SecurityConfigs {
 
         return userDetailsService;
     }
-
-    @Bean
     PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
