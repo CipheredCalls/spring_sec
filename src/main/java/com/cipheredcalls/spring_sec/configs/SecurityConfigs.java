7fdff7e6df736f2d7817b91ab632936e8c3d0faf
Mohammed Balhaddad <mbailhadadd@gmail.com>
2021-10-25 18:59:59 +0300

    finish lesson 12

--- src/main/java/com/cipheredcalls/spring_sec/configs/SecurityConfigs.java
+++ src/main/java/com/cipheredcalls/spring_sec/configs/SecurityConfigs.java
@@ -1,36 +1,37 @@
 package com.cipheredcalls.spring_sec.configs;
 
 import com.cipheredcalls.spring_sec.entities.SecurityUser;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfigs extends WebSecurityConfigurerAdapter {
 
+    DataSource dataSource ;
+
+    public SecurityConfigs(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetails())
-                .passwordEncoder(passwordEncoder());
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withUser(new SecurityUser("Mohammed","123456"))
+                .withUser(new SecurityUser("Mohammed2","12345"))
+                .withDefaultSchema()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
-    UserDetailsService userDetails(){
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        userDetailsService.createUser(new SecurityUser("Mohammed","123456"));
-        userDetailsService.createUser(new SecurityUser("Mohammed2","12345"));
-
-        return userDetailsService;
-    }
-    PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable();
     }
 
 }
